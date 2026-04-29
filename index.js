af9e84d7348ad45c69c7245faaea49cfd3cf9f82
MylloZ <lopezyllanesmatias@gmail.com>
2026-04-28 22:10:15 -0300

    Exportamos prisma

--- index.js
+++ index.js
@@ -10,13 +10,4 @@ const prisma = new PrismaClient({
     adapter,
 });
 
-main();
-
-async function main() {
-    // Aquí puedes agregar código para probar tu conexión a la base de datos o realizar operaciones CRUD.
-    prisma.tenant.findMany().then(tenants => {
-        console.log("Tenants:", tenants);
-    }).catch(error => {
-        console.error("Error fetching tenants:", error);
-    });
-}
+export default prisma;
