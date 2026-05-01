d76f9a70040cbbb6e77bf246720e964748c4fd69
MylloZ <lopezyllanesmatias@gmail.com>
2026-04-30 22:09:08 -0300

    Cambiamos el output de prisma client a generated

--- src/utils/prisma.js
+++ src/utils/prisma.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
-import pkg from "@prisma/client";
+import pkg from "../generated/client";
 const { PrismaClient } = pkg;
 import { PrismaPg } from "@prisma/adapter-pg";
 
