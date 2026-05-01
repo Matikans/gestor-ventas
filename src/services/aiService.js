b749f354919a5c058a5b76d9b378f76a3404d727
MylloZ <lopezyllanesmatias@gmail.com>
2026-05-01 20:27:16 -0300

    Cambiando modelo a gemini-flash-latest

--- src/services/aiService.js
+++ src/services/aiService.js
@@ -5,7 +5,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-const model = genAI.getGenerativeModel({model: "gemini-3.1-flash-lite-preview", generationConfig: { responseMimeType: "application/json" }});
+const model = genAI.getGenerativeModel({model: "gemini-flash-latest", generationConfig: { responseMimeType: "application/json" }});
 
 
 export const getStoreContext = async (tenantId) =>{
